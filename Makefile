3185673129e61d690669d01c812364cf6e852dda
Ben Brown <Ben@bens-mbp-6.middlebury.edu>
2016-05-09 14:08:09 -0400

    add sound playing code and resources and modify Makefile accordingly

--- Makefile
+++ Makefile
@@ -1,18 +1,20 @@
 # Makefile for CS 453
 # compile binary from multiple source files
 
-SRC = main.cpp
+SRC = main.cpp sound.cpp
 BIN = main
 
 CC = g++
 CPPFLAGS = -O2 -W -Wall -g
 LDLIBS = -lopencv_core -lopencv_highgui -lopencv_imgproc -lpng -lz
+SOUNDLIBS = -lmpg123 -lao -ldl -lm
 OBJ = $(SRC:.cpp=.o)
 
 all: $(BIN)
 
 $(BIN): $(OBJ)
-	$(CC) -o $(BIN) $(OBJ) $(LDLIBS)
+	$(CC) -o $(BIN) $(OBJ) $(LDLIBS) $(SOUNDLIBS)
+
 
 clean:
 	rm -f $(BIN) $(OBJ) core*
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include "opencv2/opencv.hpp"
+#include "sound.h"
 
 using namespace cv;
 using namespace std;
@@ -14,22 +15,31 @@ const char *win = "video";
 int main()
 {
     int cam = 0; // default camera
+    int key;
     VideoCapture cap(cam);
     if (!cap.isOpened()) {
 	fprintf(stderr, "cannot open camera %d\n", cam);
 	exit(1);
     }
 
-    namedWindow(win, 0);
+     namedWindow(win, 0);
     Mat frame;
     while (1) {
         cap >> frame;
         //DO STUFF WITH frame here
         imshow(win, frame);
-        if (waitKey(30) >= 0) // wait up to 30 msec
-	    break;
+        key = waitKey(30);
+        if (key == 107) {
+          play("snare.mp3");
+        } else if (key >= 0) {
+          break;
+        }
     }
     imwrite("frame.png", frame);
 
     return 0;
+
+  int key = waitKey(0);
+    printf("%d\n", key);
+
 }
